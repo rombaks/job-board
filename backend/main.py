a909692eaea9a5d70cb0b963852909d1d62a8b40
Roman Baksheev <rombaks.dev@gmail.com>
2023-01-12 19:07:51 +0400

    Configure static folder in main app

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from apis.general_pages.route_homepage import general_pages_router
 from core.config import settings
@@ -8,9 +9,14 @@ def include_router(app):
     app.include_router(general_pages_router)
 
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
 
 
