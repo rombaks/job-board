d944ee6ae4f3142c89e84918e820394bcbb2cf4a
Roman Baksheev <rombaks.dev@gmail.com>
2023-01-16 11:00:40 +0400

    Add jobs ORM create in repository pattern

---
+++ backend/db/repository/jobs.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+from db.models import Job
+from schemas.jobs import JobCreate
+
+
+def create_new_job(job: JobCreate, db: Session, owner_id: int):
+    job_object = Job(**job.dict(), owner_id=owner_id)
+    db.add(job_object)
+    db.commit()
+    db.refresh(job_object)
+    return job_object
