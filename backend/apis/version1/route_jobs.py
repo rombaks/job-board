3f71a1fa347cdbe22fe2e951056cc42b096d72ff
Roman Baksheev <rombaks.dev@gmail.com>
2023-01-16 23:21:09 +0400

    Add authorization to create and delete job

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from apis.version1.route_login import get_current_user_from_token
+from db.models import User
 from db.repository.jobs import (
     create_new_job,
     delete_job_by_id,
@@ -18,9 +20,12 @@ router = APIRouter()
 @router.post(
     "/create-job/", response_model=ShowJob, status_code=status.HTTP_201_CREATED
 )
-def create_job(job: JobCreate, db: Session = Depends(get_db)):
-    current_user = 1
-    job = create_new_job(job=job, db=db, owner_id=current_user)
+def create_job(
+    job: JobCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+):
+    job = create_new_job(job=job, db=db, owner_id=current_user.id)
     return job
 
 
@@ -53,12 +58,21 @@ def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
 
 
 @router.delete("/delete/{id}")
-def delete_job(id: int, db: Session = Depends(get_db)):
-    current_user_id = 1
-    message = delete_job_by_id(id=id, db=db, owner_id=current_user_id)
-    if not message:
-        raise HTTPException(
+def delete_job(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+):
+    job = retrieve_job(id=id, db=db)
+    if not job:
+        return HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Job with id {id} not found",
+            detail=f"Job with {id} does not exist",
         )
-    return {"msg": "Successfully deleted."}
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id=id, db=db, owner_id=current_user.id)
+        return {"msg": "Successfully deleted."}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="You are not permitted!!!!",
+    )
